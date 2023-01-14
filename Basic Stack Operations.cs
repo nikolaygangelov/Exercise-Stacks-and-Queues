0c7edd50162bcedc35b599d9e8f6bae4473868fe
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-14 21:53:42 +0200

    Add files via upload

---
+++ Basic Stack Operations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input1 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> input2 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Stack<int> stack = new Stack<int>(input2);
+
+            for (int i = 1; i <= input1[1]; i++)
+            {
+                stack.Pop();
+            }
+
+            if (!stack.Any())
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                if (stack.Contains(input1[2]))
+                {
+                    Console.WriteLine("true");
+                }
+                else if (!stack.Contains(input1[2]))
+                {
+                    Console.WriteLine(stack.Min());
+                }
+            }
+
+            
+
+            
+
+        }
+    }
+}
