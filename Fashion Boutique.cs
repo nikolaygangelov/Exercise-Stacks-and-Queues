cc8691c8ced48a33636b1a970d5c1fc5c58e25a5
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-16 01:23:32 +0200

    Add files via upload

---
+++ Fashion Boutique.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothes = new Stack<int>(
+                                 Console.ReadLine()
+                                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse));
+
+            int rack = int.Parse(Console.ReadLine());
+
+            int currentRack = rack;
+            int numberOfRacks = 1;
+
+            while (clothes.Any())
+            {
+                currentRack -= clothes.Peek();
+
+                if (currentRack < 0)
+                {
+                    currentRack = rack;
+                    numberOfRacks++;
+
+                    continue;
+                }
+
+                clothes.Pop();
+            }
+
+            Console.WriteLine(numberOfRacks);
+
+        }
+    }
+}
