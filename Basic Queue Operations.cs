caa894159f8e658223b1cfb888169e395910c17b
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-15 20:48:51 +0200

    Add files via upload

---
+++ Basic Queue Operations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input1 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> input2 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Queue<int> queue = new Queue<int>(input2);
+
+            for (int i = 1; i <= input1[1]; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Any())
+            {
+
+                if (queue.Contains(input1[2]))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+
+        }
+    }
+}
