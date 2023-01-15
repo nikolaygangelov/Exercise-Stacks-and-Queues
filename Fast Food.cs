4bf8680b30d69029d4318ee04746184347717cc7
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-15 20:49:35 +0200

    Add files via upload

---
+++ Fast Food.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int foodQuantity = int.Parse(Console.ReadLine());
+            int[] ordersQuantity = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> orders = new Queue<int>(ordersQuantity);
+            Console.WriteLine(orders.Max());
+
+            for (int i = 0; i < ordersQuantity.Length; i++)
+            {
+                if (orders.Count == 0)
+                {
+                    break;
+                }
+                if ((foodQuantity - orders.Peek()) <= 0)
+                {
+                    break;
+                }
+                foodQuantity -= orders.Dequeue();
+            }
+
+            if (orders.Count == 0 || (orders.Count == 0 && foodQuantity == 0))
+            {
+                Console.WriteLine($"Orders complete");
+            }
+            else if (orders.Any())
+            {
+                Console.WriteLine($"Orders left: {string.Join(' ', orders)}");
+            }
+
+        }
+    }
+}
