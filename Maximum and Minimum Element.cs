232b7d1ccd34107821c112f757a2cea23e79c066
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-15 20:49:14 +0200

    Add files via upload

---
+++ Maximum and Minimum Element.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Maximum_and_Minimum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberOfQueries = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 1; i <= numberOfQueries; i++)
+            {
+                
+                int[] inputParameters = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int commandType = inputParameters[0];
+                if (commandType == 1)
+                {
+
+                    int elementToPush = inputParameters[1];
+                    stack.Push(elementToPush);
+                    
+                }
+                else if (commandType == 2)
+                {
+                    if (stack.Count() == 0)
+                    {
+                        continue;
+                    }
+                    stack.Pop();
+                }
+                else if (commandType == 3)
+                {
+
+                    if (stack.Count() == 0)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine(stack.Max());
+
+                }
+                else if (commandType == 4)
+                {
+
+                    if (stack.Count() == 0)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine(stack.Min());
+
+                }
+
+            }
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
