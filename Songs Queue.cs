2dd40aadf606e51ba52171cc0da0c1b76d7d9613
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-18 22:36:01 +0200

    Add files via upload

---
+++ Songs Queue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine()
+                             .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> songList = new Queue<string>(songs);
+
+            while (songList.Any())
+            {
+                string commands = Console.ReadLine();
+
+                string commandType = commands.Substring(0, 4);
+                if (commandType == "Play")
+                {
+                    songList.Dequeue();
+
+                }
+                else if (commandType == "Add ")
+                {
+                    string songToAdd = string.Join(' ', commands.Skip(1));
+                    if (!songList.Contains(songToAdd))
+                    {
+                        songList.Enqueue(songToAdd);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{songToAdd} is already contained!");
+                    }
+
+                }
+                else if (commandType == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songList));
+                }
+
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
